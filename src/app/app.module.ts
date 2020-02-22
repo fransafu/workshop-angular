d00a1cd50a53446895bfa1dc1506b1011b8c7a8a
Francisco Sanchez Fuentes <fransafu@gmail.com>
2020-02-22 12:30:04 -0300

    feat(components): Add card, history and info components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { CardComponent } from './card/card.component';
+import { HistoryComponent } from './history/history.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    CardComponent,
+    HistoryComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
